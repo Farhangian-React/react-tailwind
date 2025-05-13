754e3a8b1ad0a4666dd6204aaa504af3417b6701
farhangin-elham <elhamfarhangian6@gmail.com>
2025-05-13 10:37:11 +0330

    size5

--- src/Topnav/TopNavbar.js
+++ src/Topnav/TopNavbar.js
@@ -108,7 +108,7 @@ const handleInputChange = (e) => {
              text-gray-900 text-sm rounded-lg
              hover:border-2 hover:border-orange-500 block w-full pl-10 p-2.5" placeholder="جستجو ..." required />
         </div>
-        <button  onChange={() => setOpen(true)}  type="submit" class=" flex p-2.5 ml-2 text-sm font-medium text-gray-600 bg-neutral-200 rounded-lg  hover:bg-orange-500
+        <button  onChange={() => setOpen(true)}  type="submit" class=" flex p-2.5 ml-2 text-sm font-medium text-gray-600 hover:bg-neutral-200 rounded-lg  bg-orange-500
         ">
         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -228,7 +228,7 @@ size="lg"
 <Badge color='warning' badgeContent={cartItemsBuy.length} sx={{color:"white"}}>
 <NavLink to={"/endbuy"}  style={{display:"flex",textAlign:"center", cursor:"pointer", alignItems:"center",fontWeight:600}}
 >
-<LuShoppingCart className='font-black w-7 h-6 hover:text-zinc-700 text-orange-500' />
+<LuShoppingCart className='font-black w-7 h-6 text-zinc-700 hover:text-orange-500' />
 </NavLink>
   </Badge>
 </div>
